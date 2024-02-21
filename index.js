834278b216874c9837ff12e1a3998da0279b05f4
chorsey <caylahorsey@gmail.com>
2024-02-20 19:36:37 -0500

    Completed Test

--- index.js
+++ index.js
@@ -1,1 +1,31 @@
 // Write your solution in this file!
+const employee = {
+    name: "cayla",
+    streetAddress: "18 adair",
+};
+
+
+function updateEmployeeWithKeyAndValue(employee, key, value){
+    return {
+        ...employee,
+        [key]: value,
+      };
+}; 
+
+function destructivelyUpdateEmployeeWithKeyAndValue(employee, key, value){
+     employee[key] = value;  
+     return employee;
+};
+
+
+function deleteFromEmployeeByKey(employee, key){
+    deleteFromEmployeeByKey = {...employee};
+    delete deleteFromEmployeeByKey[key];
+    return deleteFromEmployeeByKey;
+
+};
+
+function destructivelyDeleteFromEmployeeByKey(employee, key){
+    delete employee[key];
+    return employee;
+};
